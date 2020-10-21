fe3407274595c617a69a72112ca1e142d3f2e56e
azxj <1158257471@qq.com>
2020-10-21 17:09:35 +0800

    feat: use methods to get/set fields

--- Triangle/src/test/java/net/mooctest/TriangleTest.java
+++ Triangle/src/test/java/net/mooctest/TriangleTest.java
@@ -11,82 +11,93 @@ public class TriangleTest {
     return field;
   }
 
-  private static Field lborderA;
-  private static Field lborderB;
-  private static Field lborderC;
+  private long lborderA(Triangle t) throws Throwable {
+    return getField(Triangle.class, "lborderA").getLong(t);
+  }
+  private void lborderA(Triangle t, long lborderA) throws Throwable {
+	  getField(Triangle.class, "lborderA").setLong(t, lborderA);
+  }
+  private long lborderB(Triangle t) throws Throwable {
+    return getField(Triangle.class, "lborderB").getLong(t);
+  }
+  private void lborderB(Triangle t, long lborderB) throws Throwable {
+	  getField(Triangle.class, "lborderB").setLong(t, lborderB);
+  }
+  private long lborderC(Triangle t) throws Throwable {
+    return getField(Triangle.class, "lborderC").getLong(t);
+  }
+  private void lborderC(Triangle t, long lborderC) throws Throwable {
+	  getField(Triangle.class, "lborderC").setLong(t, lborderC);
+  }
 
   private Triangle t;
 
   @Before
   public void setup() throws Throwable {
-    lborderA = getField(Triangle.class, "lborderA");
-    lborderB = getField(Triangle.class, "lborderB");
-    lborderC = getField(Triangle.class, "lborderC");
-
     t = new Triangle(1, 2, 3);
   }
 
   @Test
   public void testTriangle() throws Throwable {
     t = new Triangle(1, 2, 3);
-    assertEquals(1, lborderA.getLong(t));
-    assertEquals(2, lborderB.getLong(t));
-    assertEquals(3, lborderC.getLong(t));
+    assertEquals(1, lborderA(t));
+    assertEquals(2, lborderB(t));
+    assertEquals(3, lborderC(t));
   }
 
   @Test
   public void testGetBorders() throws Throwable {
     assertArrayEquals(new long[]{
-      lborderA.getLong(t),
-      lborderB.getLong(t),
-      lborderC.getLong(t),
+      lborderA(t),
+      lborderB(t),
+      lborderC(t),
     }, t.getBorders());
   }
 
   @Test
   public void testIsTriangle1() throws Throwable {
-    lborderA.setLong(t, -1);
+    lborderA(t, -1);
     assertFalse(t.isTriangle(t));
   }
   @Test
   public void testIsTriangle2() throws Throwable {
-    lborderA.setLong(t, 1);
-    lborderB.setLong(t, 2);
-    lborderC.setLong(t, 3);
+    lborderA(t, 1);
+    lborderB(t, 2);
+    lborderC(t, 3);
     assertFalse(t.isTriangle(t));
   }
   @Test
   public void testIsTriangle3() throws Throwable {
-    lborderA.setLong(t, 1);
-    lborderB.setLong(t, 1);
-    lborderC.setLong(t, 1);
+    lborderA(t, 1);
+    lborderB(t, 1);
+    lborderC(t, 1);
     assertTrue(t.isTriangle(t));
   }
 
   @Test
   public void testGetType1() throws Throwable {
-    lborderA.setLong(t, -1);
+    lborderA(t, -1);
     assertEquals("Illegal", t.getType(t));
   }
   @Test
   public void testGetType2() throws Throwable {
-    lborderA.setLong(t, 1);
-    lborderB.setLong(t, 1);
-    lborderC.setLong(t, 1);
+    lborderA(t, 1);
+    lborderB(t, 1);
+    lborderC(t, 1);
     assertEquals("Regular", t.getType(t));
   }
   @Test
   public void testGetType3() throws Throwable {
-    lborderA.setLong(t, 2);
-    lborderB.setLong(t, 3);
-    lborderC.setLong(t, 4);
+    lborderA(t, 2);
+    lborderB(t, 3);
+    lborderC(t, 4);
     assertEquals("Scalene", t.getType(t));
   }
   @Test
   public void testGetType4() throws Throwable {
-    lborderA.setLong(t, 2);
-    lborderB.setLong(t, 2);
-    lborderC.setLong(t, 3);
+    lborderA(t, 2);
+    lborderB(t, 2);
+    lborderC(t, 3);
     assertEquals("Isosceles", t.getType(t));
   }
 
